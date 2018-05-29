b78a01b2cf5b3585b2b0a9814ec4f16e574b918b
nkechinnaji <nkechi_nnaji@gallup.com>
2018-05-28 23:51:08 -0500

    Working on Cheat Activity

---
+++ app/src/main/java/com/example/nkechi_nnaji/newquiz/view/CheatActivity.java
@@ -0,0 +1,15 @@
+package com.example.nkechi_nnaji.newquiz.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.nkechi_nnaji.newquiz.R;
+
+public class CheatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+    }
+}
